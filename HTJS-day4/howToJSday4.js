62c00e22fee9e7f6753dc265404113250f09f557
karthick2302 <62654485+karthick2302@users.noreply.github.com>
2023-12-30 14:15:40 +0530

    update

--- HTJS-day4/howToJSday4.js
+++ HTJS-day4/howToJSday4.js
@@ -1,6 +1,6 @@
 let api = 'http://www.omdbapi.com/?apikey=61e576a4&t='
-let google1 = 'https://www.google.com/search?q='
-let google2 = '&rlz=1C1CHBF_enIN1085IN1085&oq=avengers&gs_lcrp=EgZjaHJvbWUqBwgAEAAYjwIyBwgAEAAYjwIyDQgBEC4YgwEYsQMYgAQyCggCEC4YsQMYgAQyCggDEAAYsQMYgAQyCggEEC4YsQMYgAQyCggFEAAYsQMYgAQyDQgGEC4YgwEYsQMYgAQyCggHEAAYsQMYgAQyCggIEAAYsQMYgAQyCggJEC4YsQMYgATSAQk0MjY4ajBqMTWoAgCwAgA&sourceid=chrome&ie=UTF-8'
+let imdb1 = 'https://www.imdb.com/find/?q='
+let imdb2 = '&ref_=nv_sr_sm'
 
 
 function searchMovie() {
@@ -19,7 +19,7 @@ function searchMovie() {
 		document.getElementById('ratings').innerText = data.imdbRating;
 		document.getElementById('writers').innerText = data.Writer;
 		document.getElementById('poster').src = data.Poster;
-		document.getElementById('google').href = google1+data.Title+google2;
+		document.getElementById('google').href = imdb1+query+imdb2;
 		console.log(google1+data.Title+google2)
 	})
 }
